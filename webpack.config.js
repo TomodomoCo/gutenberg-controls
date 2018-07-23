03276a547b218f7967ea2e2f85c6104a3b8d1a6b
Chris Van Patten <chris@vanpattenmedia.com>
2018-07-22 20:13:43 -0400

    Add webpack config and support building

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+/**
+ * External Dependencies
+ */
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin  = require("extract-text-webpack-plugin");
+
+// CSS loader
+const cssExtractTextPlugin = new ExtractTextPlugin({
+  filename: "./build/style.css"
+});
+
+// Configuration for the ExtractTextPlugin.
+const extractConfig = {
+  use: [
+    { loader: "raw-loader" },
+    {
+      loader: "postcss-loader",
+      options: {
+        plugins: [require("autoprefixer")]
+      }
+    },
+    {
+      loader: "sass-loader"
+    }
+  ]
+};
+
+// Externals
+const externals = {
+  react: "React",
+  lodash: "lodash"
+};
+
+// WordPress dependences
+const wpDependencies = [
+  "components",
+];
+
+wpDependencies.forEach(wpDependency => {
+  externals["@wordpress/" + wpDependency] = {
+    this: ["wp", wpDependency]
+  };
+});
+
+// Webpack config
+const config = {
+  entry: "./src/index.js",
+  externals,
+  output: {
+    filename: "build/index.js",
+    path: __dirname,
+    library: ["gutenberg-blocks", "[name]"],
+    libraryTarget: "this"
+  },
+  resolve: {
+    modules: [__dirname, "node_modules"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /style\.s?css$/,
+        use: cssExtractTextPlugin.extract(extractConfig)
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["build"]),
+    cssExtractTextPlugin,
+  ],
+  stats: {
+    children: false
+  }
+};
+
+module.exports = config;
